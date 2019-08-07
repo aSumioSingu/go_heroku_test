24e1bececc3988bee8342530e8a2e5d7e538ad7b
Sumio Singu <sumio.singu@access-company.com>
2019-08-07 19:16:14 +0900

    Add Gin

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/aSumioSingu/go_heroku_test
 
 go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0 // indirect
+	github.com/heroku/x v0.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
+	if port == "" {
+		router.Run()
+	} else {
+		router.Run(":" + port)
+	}
+}
