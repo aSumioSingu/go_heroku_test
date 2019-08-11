b9f45af28623e7f8e085b4625e4229efed03575e
Sumio Singu <sumio.singu@access-company.com>
2019-08-11 17:44:26 +0900

    Use multiple templates

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
@@ -14,10 +16,10 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Static("/assets", "./public/assets")
-	router.LoadHTMLGlob("templates/*")
+	router.HTMLRender = loadTemplates()
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.tmpl", nil)
 	})
 
 	if port == "" {
@@ -25,4 +27,27 @@ func main() {
 	} else {
 		router.Run(":" + port)
 	}
+}
+
+func loadTemplates() multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	layouts, err := filepath.Glob("assets/tmpl/layouts/*.tmpl")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	includes, err := filepath.Glob("assets/tmpl/includes/*.tmpl")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Generate our templates map from our layouts/ and includes/ directories
+	for _, include := range includes {
+		layoutCopy := make([]string, len(layouts))
+		copy(layoutCopy, layouts)
+		files := append(layoutCopy, include)
+		r.AddFromFiles(filepath.Base(include), files...)
+	}
+	return r
 }
