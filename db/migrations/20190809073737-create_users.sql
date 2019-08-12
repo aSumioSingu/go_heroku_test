c33a3af906f3e35f070766bb172560cb0edf68da
Sumio Singu <sumio.singu@access-company.com>
2019-08-13 00:42:59 +0900

    Fix migration

--- db/migrations/20190809073737-create_users.sql
+++ db/migrations/20190809073737-create_users.sql
@@ -1,6 +1,12 @@
 
 -- +migrate Up
-CREATE TABLE users (id int);
+CREATE TABLE users (
+       id       int,
+       nickname varchar(255),
+       email    varchar(255));
+INSERT INTO users (id, nickname, email) VALUES ( 1, 'Ichiro', 'ichiro@email.com');
+INSERT INTO users (id, nickname, email) VALUES ( 2, 'Jiro',   'jiro@email.com');
+INSERT INTO users (id, nickname, email) VALUES ( 3, 'Saburo', 'saburo@email.com');
 
 -- +migrate Down
 DROP TABLE users;
